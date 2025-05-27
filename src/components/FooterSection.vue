fb58f981f1b665985e778742f1d962a55920b586
Imam Habib Pamungkas <imamhabibpamungkas@gmail.com>
2025-05-27 11:39:27 +0800

    perbaikan footer dan test workflow

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="text-center py-3" style="background-color: #f8f9fa;">
-    <p class="mb-0">&copy; 2024 Kasulampua. All rights reserved.</p>
+    <p class="mb-0">&copy; 2025 Kasulampua. All rights reserved.</p>
   </footer>
 </template>
 
