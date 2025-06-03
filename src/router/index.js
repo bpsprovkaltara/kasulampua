aa35befd11cc6bd6ef76452a9483a7142f2dd917
Imam Habib Pamungkas <imamhabibpamungkas@gmail.com>
2025-06-03 23:38:04 +0800

    scroll top pindah page

--- src/router/index.js
+++ src/router/index.js
@@ -59,6 +59,13 @@ const router = createRouter({
     },
 
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
